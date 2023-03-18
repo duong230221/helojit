c27cf1de28ff2743f912d389eb3d820a01e1ac76
DuongLH <duonglh@hihoay.com>
2023-03-18 22:45:43 +0700

    as

--- app/src/main/java/com/helojit/MainActivity.kt
+++ app/src/main/java/com/helojit/MainActivity.kt
@@ -2,6 +2,7 @@ package com.helojit
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.duong230221.hello.HelloJit
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
